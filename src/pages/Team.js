fafc954e5bdafe0b67a580399d1fbaa730e4a4cc
Subho808 <subhojitsaha808@gmail.com>
2025-05-02 02:19:31 +0530

    team image

--- src/pages/Team.js
+++ src/pages/Team.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import ranitImage from '../styles/images/oxyHome.png'; // Import the image
+import SubhoImage from '../styles/images/frontend.png'; // Import the image
+import MunnaImage from '../styles/images/backend.png'; // Import the image
+import RanitImage from '../styles/images/cloud.png'; // Import the image
 const Team = () => {
     return (
         <section className="team-section">
@@ -9,17 +11,17 @@ const Team = () => {
 
                 <div className="team-grid">
                     <div className="team-card">
-                        <img src="/team1.jpg" alt="Subhojit Sah" />
+                        <img src={SubhoImage} alt="Subhojit Sah" />
                         <h3>Subhojit Saha</h3>
                         <p>Frontend Developer</p>
                     </div>
                     <div className="team-card">
-                        <img src="/team2.jpg" alt="Munna Sharma" />
+                        <img src={MunnaImage} alt="Munna Sharma" />
                         <h3>Munna Sharma</h3>
                         <p>Backend Developer</p>
                     </div>
                     <div className="team-card">
-                        <img src={ranitImage} alt="Ranit Mondal" />
+                        <img src={RanitImage} alt="Ranit Mondal" />
                         <h3>Ranit Mondal</h3>
                         <p>Backend Developer & Cloud Engineer</p>
                         <p></p>
